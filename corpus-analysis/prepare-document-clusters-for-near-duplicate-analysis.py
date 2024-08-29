207c39056dad903006e0b8f8dbbbd3cc6a5d6c8b
Maik Froebe <maik.froebe@uni-jena.de>
2024-08-29 06:08:39 +0200

    mf

--- corpus-analysis/prepare-document-clusters-for-near-duplicate-analysis.py
+++ corpus-analysis/prepare-document-clusters-for-near-duplicate-analysis.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 from tira.third_party_integrations import ir_datasets
+import gzip
+import json
+from tqdm import tqdm
+import csv
 
 DS = [
     'longeval-2023-01-20240423-training',
@@ -11,5 +15,23 @@ DS = [
     'longeval-train-20230513-training'
 ]
 
-for d in DS:
-    ir_datasets.load(f'ir-benchmarks/{d}').docs_store()
+DS = [ir_datasets.load(f'ir-benchmarks/{i}').docs_store() for i in tqdm(DS, 'Load docs_stores')]
+
+def default_text(docid):
+    for d in DS:
+        if docid in d:
+            return d[docid].default_text()
+
+    raise ValueError('Could not find docid:', docid)
+
+with gzip.open('../data/document-groups-judged.csv.gz', 'rt') as f, open('../data/document-groups.jsonl', 'w') as outp:
+    for l in csv.reader(f):
+        url = l[0]
+        l.remove(url)
+        assert len(l) == 6
+        l = [i for i in l if i]
+
+        if len(l) <= 1:
+            continue
+        outp.write(json.dumps({'url': url, 'docs': {i: default_text(i) for i in l}}) + '\n')
+
