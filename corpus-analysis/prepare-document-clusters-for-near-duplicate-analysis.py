e70f1c2eb6e4377ad622942fe28eed60358a7297
Maik Froebe <maik.froebe@uni-jena.de>
2024-08-29 06:54:06 +0200

    mf

--- corpus-analysis/prepare-document-clusters-for-near-duplicate-analysis.py
+++ corpus-analysis/prepare-document-clusters-for-near-duplicate-analysis.py
@@ -5,6 +5,19 @@ import json
 from tqdm import tqdm
 import csv
 
+def all_doc_groups():
+    with gzip.open('../data/document-groups-judged.csv.gz', 'rt') as f:
+        for l in csv.reader(f):
+            url = l[0]
+            l.remove(url)
+            assert len(l) == 6
+            l = [i for i in l if i]
+
+            if len(l) <= 1:
+                continue
+            yield url, l
+
+
 DS = [
     'longeval-2023-01-20240423-training',
     'longeval-2023-06-20240418-training',
@@ -15,23 +28,16 @@ DS = [
     'longeval-train-20230513-training'
 ]
 
-DS = [ir_datasets.load(f'ir-benchmarks/{i}').docs_store() for i in tqdm(DS, 'Load docs_stores')]
+docs = {}
+for d in tqdm(DS, 'Load docs_stores'):
+    d = ir_datasets.load(f'ir-benchmarks/{d}').docs_store()
+    for _, i in all_doc_groups():
+        for j in i:
+            if j in d:
+                docs[j] = d.default_text()
 
-def default_text(docid):
-    for d in DS:
-        if docid in d:
-            return d[docid].default_text()
 
-    raise ValueError('Could not find docid:', docid)
-
-with gzip.open('../data/document-groups-judged.csv.gz', 'rt') as f, open('../data/document-groups.jsonl', 'w') as outp:
-    for l in csv.reader(f):
-        url = l[0]
-        l.remove(url)
-        assert len(l) == 6
-        l = [i for i in l if i]
-
-        if len(l) <= 1:
-            continue
-        outp.write(json.dumps({'url': url, 'docs': {i: default_text(i) for i in l}}) + '\n')
+with open('../data/document-groups.jsonl', 'w') as outp:
+    for url, l in all_doc_groups():
+        outp.write(json.dumps({'url': url, 'docs': {i: docs[i] for i in l}}) + '\n')
 
